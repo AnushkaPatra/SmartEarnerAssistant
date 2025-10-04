b3569b10bd15de1c5c093ea1b836af76e26c87f1
Tanisha Doshi <doshitanu2006@gmail.com>
2025-10-04 20:34:48 +0200

    finished well being

--- application/wellbeing.py
+++ application/wellbeing.py
@@ -1,4 +1,21 @@
-def calculate_driver_wellbeing_score():
-    print(5)
+def ride_score(duration):
+    """
+    Returns a score from 1 to 5 based on ride duration thresholds:
+    Thresholds:
+    1: 0-21
+    2: 22-43
+    3: 44-65
+    4: 66-87
+    5: 88-110
+    """
+    if 0 <= duration <= 21:
+        return 1
+    elif 22 <= duration <= 43:
+        return 2
+    elif 44 <= duration <= 65:
+        return 3
+    elif 66 <= duration <= 87:
+        return 4
+    else:
+        return 5
 
-calculate_driver_wellbeing_score()
